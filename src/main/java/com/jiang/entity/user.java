e5a677a559fe04bee21a065801762493fa74f1c0
xiaoming <2839380667@qq.com>
2020-12-15 12:15:20 +0800

    第二次git推送

--- src/main/java/com/jiang/entity/user.java
+++ src/main/java/com/jiang/entity/user.java
@@ -3,4 +3,11 @@ package com.jiang.entity;
 public class user {
     private Integer id;
     private String name;
+    private String sex;
+
+    public user(Integer id, String name, String sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
 }
