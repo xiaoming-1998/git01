07443922ab14cb6bcb1dc1c808e277d039b97ec0
xiaoming <2839380667@qq.com>
2020-12-15 12:22:24 +0800

    第三次git推送

--- src/main/java/com/jiang/entity/user.java
+++ src/main/java/com/jiang/entity/user.java
@@ -10,4 +10,28 @@ public class user {
         this.name = name;
         this.sex = sex;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
